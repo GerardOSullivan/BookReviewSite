94f80b7e25e123765f8be3db4bb3bcdfe95f4863
GerardOSullivan <58072698+GerardOSullivan@users.noreply.github.com>
2021-11-29 15:16:05 +0000

    Update Books.js
    I hard codded in the API data for testing

--- app_server/controllers/books.js
+++ app_server/controllers/books.js
@@ -14,7 +14,31 @@ const _renderHomepage = function(req, res, responseBody){
                 title: 'Book Reviews',
                 strapline: 'Find accurate reviews on your favourite books'
             },
-            books: responseBody
+            books: [{
+                name:'Clean Code',
+                description: 'What Is Clean Code? The code can be measured with either "good" or "bad" in the code review or by how ...',
+                image: '/images/cleancode.jpg'
+            },{
+                name:'Software Testing',
+                description: 'To learn about software-testing job opportunities and practice with sample scripts on how to automate software ...',
+                image: '/images/softwaretesting.jpg'
+            },{
+                name:'Data Structures and Algorithms',
+                description: 'In data science, algorithms and data structures make up the function and storage of data collecting. While coding  ...',
+                image: '/images/DataStructures.jpg'
+            },{
+                name:'Data Structures in Python',
+                description: 'The book explains how computers use algorithms to solve problems, creating many of the technology tasks we ...',
+                image: '/images/DataStructures2.jpg'
+            },{
+                name:'Object Oriented Software Engineering',
+                description: 'Object-oriented programming (OOP) is a programming paradigm based on the concept of "objects", which can ...',
+                image: '/images/OOAD.jpg'
+            },{
+                name:'Object Oriented Analysis',
+                description: 'Object-oriented analysis and design (OOAD) is a technical approach for analyzing and designing an ...',
+                image: '/images/OOAD2.jpg'
+            }] 
             });
 };
 
@@ -24,7 +48,7 @@ const  homepage = function(req, res){
     const requestOptions = { 
     url : apiOptions.server + path, 
     method : 'GET', 
-    json : {}
+    json : {},
     }; 
     request(requestOptions, (err, response, body) => { 
     _renderHomepage(req, res, body); 
